25bbc85c418d434f70c91281c45ada36e715db2c
JuanCarlosAraujo <jcarlosaraujo@unicesar.edu.co>
2023-05-12 00:30:23 -0500

    Andres Arreglalo

--- JATA/Red.py
+++ JATA/Red.py
@@ -5,150 +5,116 @@ import numpy as np
 
 
 
-def Red():
-    errorPatron = 0
-    funcionActivacion = ""
-    sumatoriaErroresPatron = 0
-    errorPermitido = 0.1
-    rataAprendizaje = 0.1
-    numeroInteracciones = 0
-    salidaSistema = [0] * 3
-    patrones = []
-    pesos=[]
-    umbrales=[]
-    erroresSalida=[0] * 3
-    salidaOriginal=[]
+class Red():
+    def __init__(self, rata, iteraciones,funActivacion,errorPermitido):
+
+        self.errorPatron = 0
+        self.funcionActivacion = funActivacion
+        self.sumatoriaErroresPatron = 0
+        self.errorPermitido = errorPermitido
+        self.rataAprendizaje = rata
+        self.numeroInteracciones = iteraciones
+        self.salidaSistema = []
+        self.patrones = []
+        self.pesos=[]
+        self.umbrales=[]
+        self.erroresSalida=[]
+        self.salidaOriginal=[]
+        self.numeroEntradas = 0
+        self.numeroSalidas = 0
  
-    #calcular valor de las salidas
-    get_output(salidaOriginal, salidaSistema, patrones, pesos, umbrales)
-
-    #Calcular Error por salida
-    output_error(erroresSalida,salidaOriginal,salidaSistema)
-
-    #Calcular el error por patron
-    pattern_error(erroresSalida, salidaOriginal, errorPatron)
-
-    #Actualizar pesos
-    update_weights(pesos, salidaOriginal, patrones, rataAprendizaje, erroresSalida)
-
-    #Actualizar umbrales
-    update_thresholds(umbrales, salidaOriginal, rataAprendizaje, erroresSalida)
-
-    #Sumatoria y comprobacion de error
-    error_checking(sumatoriaErroresPatron, errorPermitido)
-
-    #Analisis de todos los patrones 
-    analysis_of_all_patterns(salidaOriginal, salidaSistema, patrones, pesos, umbrales, erroresSalida, errorPatron, sumatoriaErroresPatron, rataAprendizaje, errorPermitido)
-
-    #Repetir patrones segun el numero de interacciones
-    patterns_according_to_interactions(salidaOriginal, salidaSistema, patrones, pesos, umbrales, erroresSalida, 
-                                       errorPatron, sumatoriaErroresPatron, rataAprendizaje, errorPermitido, numeroInteracciones)
+    def get_output(self,entradas):
+        print(self.pesos)
+        salida= [0] * self.numeroSalidas
+        for i in range(self.numeroSalidas):
+            print("salida ", i)
+            for j in range(len(entradas)):
+                print("datos actuales:", entradas[j], self.pesos[j][i], self.umbrales[i])
+                salida[i] += (entradas[j] * self.pesos[j][i]) 
+                print("resultado: ", salida[i])
+            salida[i] = salida[i] - self.umbrales[i]
+        return salida
     
-    #Cargar archivo de patrones
-    file_upload(patrones, salidaOriginal)
+    def output_error(self):
 
-    #Funcion de activacion
-    activation_funcion(funcionActivacion, salidaSistema)
+        self.erroresSalida = [0] * self.numeroSalidas
 
-    #Creacion de pesos y umbrales aleatoriamente
-    create_weights_thresholds(patrones, salidaOriginal, pesos, umbrales)
+        for i in range(self.numeroSalidas):
+            self.erroresSalida[i] = self.salidaOriginal[i] - self.salidaSistema[i]
 
+    def pattern_error(self):
+        for valor in self.erroresSalida:
+            errorPatron = errorPatron + abs(valor)
+        errorPatron = errorPatron / (len(self.salidaOriginal))
+        return errorPatron
 
-def get_output(salidaOriginal, salidaSistema, entradas, pesos, umbrales):
-    print("calculando salida...")
-    for i in range(len(salidaOriginal)):
-        print("salida ", i)
-        for j in range(len(entradas)):
-            print("datos actuales:", entradas[j], pesos[j][i], umbrales[i])
-            salidaSistema[i] += (entradas[j] * pesos[j][i]) 
-            print("resultado: ", salidaSistema[i])
-        salidaSistema[i] = salidaSistema[i] - umbrales[i]
+    def update_weights(self,rataAprendizaje):
+        for i in range(len(self.salidaOriginal)):
+            for j in range(len(self.patrones)):
+                self.pesos[j][i] = self.pesos[j][i] + rataAprendizaje * self.erroresSalida[i] * self.entradas[j]
 
-def output_error(erroresSalida, salidaOriginal, salidaSistema):
-    for i in range(len(salidaOriginal)):
-        erroresSalida[i] = salidaOriginal[i] - salidaSistema[i]
+    def update_thresholds(self,rataAprendizaje):
+        for i in range(len(self.salidaOriginal)):
+            self.umbrales[i] = self.umbrales[i] + rataAprendizaje * self.erroresSalida[i] * 1
 
-def pattern_error(erroresSalida, salidaOriginal, errorPatron):
-    for valor in erroresSalida:
-        errorPatron = errorPatron + abs(valor)
-    errorPatron = errorPatron / (len(salidaOriginal))
-    return errorPatron
+    def error_checking(self, errorPermitido):
+        if(self.sumatoriaErroresPatron <= errorPermitido):
+            return True
+        else:
+            return False
 
-def update_weights(pesos, salidaOriginal, entradas, rataAprendizaje, erroresSalida):
-    for i in range(len(salidaOriginal)):
-        for j in range(len(entradas)):
-            pesos[j][i] = pesos[j][i] + rataAprendizaje * erroresSalida[i] * entradas[j]
+    def analysis_of_all_patterns(self,funcionActivacion, rataAprendizaje, errorPermitido):
+        for i in range(len(self.patrones)): 
+            columna = [fila[i] for fila in self.patrones]
+            self.salidaSistema.append(self.get_output(columna))
+            self.output_error()
+            sumatoriaErroresPatron = self.pattern_error() + sumatoriaErroresPatron
+            self.update_weights(rataAprendizaje)
+            self.update_thresholds(rataAprendizaje)
+        self.activation_funcion(funcionActivacion)
+        return self.error_checking(errorPermitido)
 
-def update_thresholds(umbrales, salidaOriginal, rataAprendizaje, erroresSalida):
-    for i in range(len(salidaOriginal)):
-        umbrales[i] = umbrales[i] + rataAprendizaje * erroresSalida[i] * 1
+    def patterns_according_to_interactions(self,funcionActivacion, rataAprendizaje, errorPermitido, numeroInteracciones):
+        for i in range(numeroInteracciones):
+            if (self.analysis_of_all_patterns(funcionActivacion,rataAprendizaje, errorPermitido) == True):
+                return i
+        return numeroInteracciones
 
-def error_checking(sumatoriaErroresPatron, errorPermitido):
-    if(sumatoriaErroresPatron <= errorPermitido):
-        return True
-    else:
-        return False
+    def file_upload(self,filename):
 
-def analysis_of_all_patterns(salidaOriginal, salidaSistema, patrones, pesos, umbrales, erroresSalida, errorPatron, sumatoriaErroresPatron, rataAprendizaje, errorPermitido):
-    for i in range(len(patrones)):
-        get_output(salidaOriginal, salidaSistema, patrones, pesos, umbrales)
-        output_error(erroresSalida,salidaOriginal,salidaSistema)
-        sumatoriaErroresPatron = pattern_error(erroresSalida, salidaOriginal, errorPatron) + sumatoriaErroresPatron
-        update_weights(pesos, salidaOriginal, patrones, rataAprendizaje, erroresSalida)
-        update_thresholds(umbrales, salidaOriginal, rataAprendizaje, erroresSalida)
-    return error_checking(sumatoriaErroresPatron, errorPermitido)
+        matriz = np.loadtxt(filename)
+        columnas = len(matriz[0])
+        filas = len(matriz)
 
-def patterns_according_to_interactions(salidaOriginal, salidaSistema, patrones, pesos, umbrales, erroresSalida, 
-                                       errorPatron, sumatoriaErroresPatron, rataAprendizaje, errorPermitido, numeroInteracciones):
-    for i in range(numeroInteracciones):
-        if (analysis_of_all_patterns(salidaOriginal, salidaSistema, patrones, pesos, umbrales, erroresSalida, errorPatron, sumatoriaErroresPatron, rataAprendizaje, errorPermitido) == True):
-            return i
-    return numeroInteracciones    
-
-def file_upload(patrones, salidaOriginal):
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename()
-    filename = file_path 
-    matriz = np.loadtxt(filename)
-    columnas = len(matriz[0])
-    filas = len(matriz)
-
-    for i in range(columnas):
-        filaEntradas = []
-        filaSalidas = []
-        if(matriz[0][i] == 1):
-            for j in range(1, filas):
-                a = matriz[j][i]
-                filaEntradas.append(a)
-            patrones.append(filaEntradas)
-        
-        if(matriz[0][i] == 0):
-            for j in range(1, filas):
-                b = matriz[j][i]
-                filaSalidas.append(b)
-            salidaOriginal.append(filaSalidas)
-
-def activation_funcion(funcionActivacion, salidaSistema):
-    columnas = len(salidaSistema[0])
-    filas = len(salidaSistema)
-    if(funcionActivacion == "escalon"):
         for i in range(columnas):
-            for j in range(1, filas):
-                if(salidaSistema[j][i] < 0):
-                    salidaSistema[j][i] == 0
-                else:
-                    salidaSistema[j][i] == 1
+            filaEntradas = []
+            filaSalidas = []
+            if(matriz[0][i] == 1):
+                for j in range(1, filas):
+                    a = matriz[j][i]
+                    filaEntradas.append(a)
+                self.patrones.append(filaEntradas)
+            
+            if(matriz[0][i] == 0):
+                for j in range(1, filas):
+                    b = matriz[j][i]
+                    filaSalidas.append(b)
+                self.salidaOriginal.append(filaSalidas)
+        self.create_weights_thresholds()
 
-def create_weights_thresholds(patrones, salidaOriginal, pesos, umbrales):
-    numeroEntradas = len(patrones)
-    numeroSalidas = len(salidaOriginal)
-    pesos = [[random.uniform(0, 1) for j in range(numeroSalidas)] for i in range(numeroEntradas)]
-    umbrales = [random.uniform(0, 1) for j in range(numeroSalidas)]
+    def activation_funcion(self,funcionActivacion):
+        columnas = len(self.salidaSistema[0])
+        filas = len(self.salidaSistema)
+        if(funcionActivacion == "Perceptron"):
+            for i in range(columnas):
+                for j in range(1, filas):
+                    if(self.salidaSistema[j][i] < 0):
+                        self.salidaSistema[j][i] == 0
+                    else:
+                        self.salidaSistema[j][i] == 1
 
-        
-
-    
-
-
-
+    def create_weights_thresholds(self):
+        self.numeroEntradas = len(self.patrones)
+        self.numeroSalidas = len(self.salidaOriginal)
+        self.pesos = [[random.uniform(0, 1) for j in range(self.numeroSalidas)] for i in range(self.numeroEntradas)]
+        self.umbrales = [random.uniform(0, 1) for j in range(self.numeroSalidas)]
